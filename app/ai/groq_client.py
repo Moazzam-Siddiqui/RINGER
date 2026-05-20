9085b5137ec484302f9dd91abb16388d00cdae16
Moazzam Ahmad Siddiqui <moazzamdubai12345@gmail.com>
2026-05-20 22:20:18 +0530

    Initial Commit

---
+++ app/ai/groq_client.py
@@ -0,0 +1,34 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+os.getenv("GROQ2_API_KEY")
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+
+def generate_reply(message):
+    response = client.chat.completions.create(
+        model="llama3-8b-8192",
+
+        messages=[
+            {
+                "role": "system",
+                "content": (
+                    "You are Moazzam's AI phone assistant. "
+                    "Talk casually and briefly."
+                )
+            },
+
+            {
+                "role": "user",
+                "content": message
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
