e0ad8a4ef4bdfae7555a17837c59eb4a9de172dc
MEOW-CODER <67272536+Moazzam-Siddiqui@users.noreply.github.com>
2026-05-21 19:55:00 +0530

    Add files via upload

--- app/ai/groq_client.py
+++ app/ai/groq_client.py
@@ -7,13 +7,13 @@ load_dotenv()
 os.getenv("GROQ2_API_KEY")
 
 client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
+    api_key=os.getenv("GROQ2_API_KEY")
 )
 
 
 def generate_reply(message):
     response = client.chat.completions.create(
-        model="llama3-8b-8192",
+        model="llama-3.1-8b-instant",
 
         messages=[
             {
