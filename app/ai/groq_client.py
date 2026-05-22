2c2dd2893cd1c665d6cb092efbab91d2f40c2ad4
MEOW-CODER <67272536+Moazzam-Siddiqui@users.noreply.github.com>
2026-05-22 21:29:48 +0530

    Update groq_client.py

--- app/ai/groq_client.py
+++ app/ai/groq_client.py
@@ -1,6 +1,7 @@
 from groq import Groq
 from dotenv import load_dotenv
 import os
+from app.storage.save_chat import save_message
 
 load_dotenv()
 
@@ -34,6 +35,7 @@ def generate_reply(message):
     )
 
     ai_reply = response.choices[0].message.content
+    save_message("assistant", ai_reply)
 
     conversation_history.append(
         {
@@ -41,5 +43,6 @@ def generate_reply(message):
             "content": ai_reply
         }
     )
+    save_message("user", message)
 
     return ai_reply
