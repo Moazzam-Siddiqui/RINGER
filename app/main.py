6840dc923babbad5424173a20edb028eb8c895ed
MEOW-CODER <67272536+Moazzam-Siddiqui@users.noreply.github.com>
2026-05-24 16:48:30 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from app.routes.chat import router as chat_router
 from app.routes.audio import router as audio_router
+from app.routes.voice_agent import router as voice_router
 
 app = FastAPI()
 app.include_router(audio_router)
 app.include_router(chat_router)
+app.include_router(voice_router)
 
 @app.get("/")
 async def root():
