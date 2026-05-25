bc1239b97f85b7ae52efae1416a6dd063b53db1f
MEOW-CODER <67272536+Moazzam-Siddiqui@users.noreply.github.com>
2026-05-25 23:07:31 +0530

    Update voice_agent.py

--- app/routes/voice_agent.py
+++ app/routes/voice_agent.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter
 from app.audio.transcribe import transcribe_audio
 from app.ai.groq_client import generate_reply
 from app.audio.speak import text_to_speech
+from app.agent.priority_detector import detect_priority
+
 
 router = APIRouter()
 
@@ -12,15 +14,17 @@ async def voice_agent():
     user_text = transcribe_audio(
         "recordings/test.mp3"
     )
+    priority = detect_priority(user_text)
 
     ai_reply = generate_reply(
     user_text,
-    caller_name=" rahul "
+    caller_name="Elma "
 )
     audio_path = text_to_speech(ai_reply)
 
     return {
     "transcription": user_text,
+    "priority": priority,
     "ai_reply": ai_reply,
     "audio_file": audio_path
 }
